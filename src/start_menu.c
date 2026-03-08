9e735509ce30350df8b8b933f8ae7dcea21a43b7
FelipeBallesteros0 <felipe1024@gmail.com>
2026-03-08 17:48:23 -0300

    Make start menu button labels transparent overlay

--- src/start_menu.c
+++ src/start_menu.c
@@ -288,6 +288,7 @@ static const struct WindowTemplate sWindowTemplate_StartMenuButtonsText =
     .paletteNum = 15,
     .baseBlock = START_MENU_BUTTON_TEXT_BASEBLOCK
 };
+static const u8 sStartMenuButtonTextColors[] = {TEXT_COLOR_TRANSPARENT, TEXT_COLOR_WHITE, TEXT_COLOR_DARK_GRAY};
 
 // Local functions
 static void BuildStartMenuActions(void);
@@ -1682,7 +1683,7 @@ static void StartMenu_DrawButtonText(void)
         width = GetStringWidth(FONT_NORMAL, label, 0);
         x = (slot % 3) * 64 + ((64 - width) / 2);
         y = (slot / 3) * 32 + 12;
-        AddTextPrinterParameterized(sStartMenuButtonTextWindowId, FONT_NORMAL, label, x, y, TEXT_SKIP_DRAW, NULL);
+        AddTextPrinterParameterized3(sStartMenuButtonTextWindowId, FONT_NORMAL, x, y, sStartMenuButtonTextColors, TEXT_SKIP_DRAW, label);
     }
 
     PutWindowTilemap(sStartMenuButtonTextWindowId);
